2316de12e7f079c709096f0febecbf35a34bfd5f
Pawel Lewandowski <pawel@flatterthanearth.com>
2019-05-01 16:40:41 +0200

    Revert "Remove old test sub project"
    This reverts commit f79a12073fee8230e886934c378a7fa9df1aa8be.

---
+++ SW.API.Integration.Test/Scenarios/PlanetControllerTests.cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.Json;
+using SW.API.Integration.Test.Fixtures;
+using SW.Business.DTO;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SW.API.Integration.Test.Scenarios
+{
+    public class PlanetControllerTests : IClassFixture<TestServerFixture>
+    {
+
+        private readonly TestServerFixture _fixture;
+
+        public PlanetControllerTests(TestServerFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+
+        [Fact]
+        public async Task GetShouldReturnStatusCode200()
+        {
+            var response = await _fixture.Client.GetAsync("api/Planet?page=1&count=1");
+
+            Assert.True(response.IsSuccessStatusCode);
+        }
+
+
+
+
+        [Fact]
+        public async Task PostPlanetWithoutNameReturnBadRequest()
+        {
+            PlanetAddDTO addPlanetDto = new PlanetAddDTO();
+            addPlanetDto.Name = "";
+
+
+            var content = new StringContent(JsonConvert.SerializeObject(addPlanetDto), Encoding.UTF8, "application/json");
+            var response = await _fixture.Client.PostAsync("api/Planet/", content);
+
+            Assert.True(response.StatusCode == System.Net.HttpStatusCode.BadRequest);
+        }
+
+
+
+        [Fact]
+        public async Task GetPlanetThatNotExistReturnBadRequest()
+        {
+            var response = await _fixture.Client.GetAsync("api/Planet/-1");
+
+            Assert.True(response.StatusCode == System.Net.HttpStatusCode.BadRequest);
+        }
+
+
+
+
+    }
+}
